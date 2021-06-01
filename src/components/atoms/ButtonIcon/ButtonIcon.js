11e6381ede25be897f9badf13a2b3222e00977b5
Piwko Marcin <piwkoq@gmail.com>
2021-06-01 17:53:14 +0200

    Updated css for component class

--- src/components/atoms/ButtonIcon/ButtonIcon.js
+++ src/components/atoms/ButtonIcon/ButtonIcon.js
@@ -1,15 +1,19 @@
 import styled from 'styled-components';
 
 const ButtonIcon = styled.button`
+  display: block;
   border: none;
   border-radius: 20px;
   width: 67px;
   height: 67px;
   background-image: url(${({ icon }) => icon});
-  background-size: 40px;
+  background-size: 40%;
   background-position: 50% 50%;
   background-repeat: no-repeat;
-  background-color: ${({ active }) => (active ? 'white' : 'transparent')};
+
+  &.active {
+    background-color: white;
+  }
 `;
 
 export default ButtonIcon;
