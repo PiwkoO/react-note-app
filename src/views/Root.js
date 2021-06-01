447e7f7ef1e00754e9679aa0d1ac08f626aaabec
Piwko Marcin <piwkoq@gmail.com>
2021-06-01 17:48:14 +0200

    Imported and added react-route

---
+++ src/views/Root.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MainTemplate from 'templates/MainTemplate';
+import Notes from './Notes';
+import Articles from './Articles';
+import Twitters from './Twitters';
+
+const Root = () => (
+  <BrowserRouter>
+    <MainTemplate>
+      <Switch>
+        <Route exact path="/" component={Notes} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/twitters" component={Twitters} />
+      </Switch>
+    </MainTemplate>
+  </BrowserRouter>
+);
+
+export default Root;
