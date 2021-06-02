47b7d2a7bdee8bfcddbf2ce7595d1b79e598e17c
Piwko Marcin <piwkoq@gmail.com>
2021-06-02 21:41:55 +0200

    Imported and added react-redux and store

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'store';
 import MainTemplate from 'templates/MainTemplate';
 import { routes } from 'routes';
 import Notes from './Notes';
@@ -8,23 +10,25 @@ import Twitters from './Twitters';
 import DetailsPage from './DetailsPage';
 
 const Root = () => (
-  <BrowserRouter>
-    <MainTemplate>
-      <Switch>
-        <Route
-          exact
-          path={routes.home}
-          render={() => <Redirect to="/notes" />}
-        />
-        <Route exact path={routes.notes} component={Notes} />
-        <Route path={routes.note} component={DetailsPage} />
-        <Route exact path={routes.twitters} component={Twitters} />
-        <Route path={routes.twitter} component={DetailsPage} />
-        <Route exact path={routes.articles} component={Articles} />
-        <Route path={routes.article} component={DetailsPage} />
-      </Switch>
-    </MainTemplate>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <MainTemplate>
+        <Switch>
+          <Route
+            exact
+            path={routes.home}
+            render={() => <Redirect to="/notes" />}
+          />
+          <Route exact path={routes.notes} component={Notes} />
+          <Route path={routes.note} component={DetailsPage} />
+          <Route exact path={routes.twitters} component={Twitters} />
+          <Route path={routes.twitter} component={DetailsPage} />
+          <Route exact path={routes.articles} component={Articles} />
+          <Route path={routes.article} component={DetailsPage} />
+        </Switch>
+      </MainTemplate>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default Root;
