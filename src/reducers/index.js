7e079c5b03ea0c0e5aaffff8dbc2f77e2d04848e
Piwko Marcin <piwkoq@gmail.com>
2021-06-02 21:44:05 +0200

    Created reducer for redux

---
+++ src/reducers/index.js
@@ -0,0 +1,114 @@
+const initialState = {
+  notes: [
+    {
+      id: 1,
+      title: 'Wake me up when Vue ends',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+    },
+    {
+      id: 2,
+      title: 'Como es An Gular?',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+    },
+    {
+      id: 3,
+      title: 'Du bist Reactish',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '5 days',
+    },
+    {
+      id: 4,
+      title: 'Reactuj się kto moze!',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '10 days',
+    },
+  ],
+  twitters: [
+    {
+      id: 1,
+      title: 'Hello Roman',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+      twitterName:
+        'https://pbs.twimg.com/profile_images/1104491562854158336/A-NTwQhW_400x400.png',
+    },
+    {
+      id: 2,
+      title: 'Redux guy',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+      twitterName:
+        'https://pbs.twimg.com/profile_images/1336281436685541376/fRSl8uJP_400x400.jpg',
+    },
+    {
+      id: 3,
+      title: 'React router stuff',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '5 days',
+      twitterName:
+        'https://pbs.twimg.com/profile_images/1095819845382299649/zG-2_UHS_400x400.jpg',
+    },
+    {
+      id: 4,
+      title: 'Super animacje!',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '10 days',
+      twitterName:
+        'https://pbs.twimg.com/profile_images/1281071936605323266/wc1KRZLK_400x400.jpg',
+    },
+  ],
+  articles: [
+    {
+      id: 1,
+      title: 'React on my mind',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '1 day',
+    },
+    {
+      id: 2,
+      title: 'Wish you React',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '1 day',
+    },
+    {
+      id: 3,
+      title: 'You gave React a bad name',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '5 days',
+    },
+  ],
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'REMOVE_ITEM':
+      return {
+        ...state,
+        [action.payload.itemType]: [
+          ...state[action.payload.itemType].filter(
+            (item) => item.id !== action.payload.id
+          ),
+        ],
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
