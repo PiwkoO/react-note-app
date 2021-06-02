a01d82051c66ebf072b80cd9c9b8e4f0d5992362
Piwko Marcin <piwkoq@gmail.com>
2021-06-02 15:34:29 +0200

    Created new structure and added some placeholder data

--- src/views/Articles.js
+++ src/views/Articles.js
@@ -1,10 +1,56 @@
 import React from 'react';
-import UserPageTemplate from 'templates/UserPageTemplate';
+import GridTemplate from 'templates/GridTemplate';
+import Card from 'components/molecules/Card/Card';
+
+const articles = [
+  {
+    id: 1,
+    title: 'React on my mind',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    articleUrl: 'https://youtube.com/helloroman',
+    created: '1 day',
+  },
+  {
+    id: 2,
+    title: 'Wish you React',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    articleUrl: 'https://youtube.com/helloroman',
+    created: '1 day',
+  },
+  {
+    id: 3,
+    title: 'You gave React a bad name',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    articleUrl: 'https://youtube.com/helloroman',
+    created: '5 days',
+  },
+  {
+    id: 4,
+    title: 'Is it React you looking for?',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    articleUrl: 'https://youtube.com/helloroman',
+    created: '10 days',
+  },
+];
 
 const Articles = () => (
-  <UserPageTemplate pageType="article">
-    <h1>Articles</h1>
-  </UserPageTemplate>
+  <GridTemplate pageType="articles">
+    {articles.map(({ id, title, content, created, articleUrl }) => (
+      <Card
+        cardType="articles"
+        id={id}
+        title={title}
+        content={content}
+        articleUrl={articleUrl}
+        created={created}
+        key={id}
+      />
+    ))}
+  </GridTemplate>
 );
 
 export default Articles;
--- src/views/Twitters.js
+++ src/views/Twitters.js
@@ -1,10 +1,60 @@
 import React from 'react';
-import UserPageTemplate from 'templates/UserPageTemplate';
+import GridTemplate from 'templates/GridTemplate';
+import Card from 'components/molecules/Card/Card';
+
+const twitters = [
+  {
+    id: 1,
+    title: 'Hello Roman',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '1 day',
+    twitterName:
+      'https://pbs.twimg.com/profile_images/1104491562854158336/A-NTwQhW_400x400.png',
+  },
+  {
+    id: 2,
+    title: 'Redux guy',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '1 day',
+    twitterName:
+      'https://pbs.twimg.com/profile_images/1336281436685541376/fRSl8uJP_400x400.jpg',
+  },
+  {
+    id: 3,
+    title: 'React router stuff',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '5 days',
+    twitterName:
+      'https://pbs.twimg.com/profile_images/1095819845382299649/zG-2_UHS_400x400.jpg',
+  },
+  {
+    id: 4,
+    title: 'Super animacje!',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '10 days',
+    twitterName:
+      'https://pbs.twimg.com/profile_images/1281071936605323266/wc1KRZLK_400x400.jpg',
+  },
+];
 
 const Twitters = () => (
-  <UserPageTemplate pageType="twitter">
-    <h1>Twitters</h1>
-  </UserPageTemplate>
+  <GridTemplate pageType="twitters">
+    {twitters.map(({ id, title, content, created, twitterName }) => (
+      <Card
+        cardType="twitters"
+        id={id}
+        title={title}
+        content={content}
+        twitterName={twitterName}
+        created={created}
+        key={id}
+      />
+    ))}
+  </GridTemplate>
 );
 
 export default Twitters;
