a45144f8faa38893af5031f7800ae3fb6eddc4ce
Piwko Marcin <piwkoq@gmail.com>
2021-06-06 17:59:30 +0200

    Updated data and added new action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,28 +1,28 @@
 const initialState = {
   notes: [
     {
-      id: 1,
+      id: '1',
       title: 'Wake me up when Vue ends',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
       created: '1 day',
     },
     {
-      id: 2,
+      id: '2',
       title: 'Como es An Gular?',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
       created: '1 day',
     },
     {
-      id: 3,
+      id: '3',
       title: 'Du bist Reactish',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
       created: '5 days',
     },
     {
-      id: 4,
+      id: '4',
       title: 'Reactuj się kto moze!',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
@@ -31,7 +31,7 @@ const initialState = {
   ],
   twitters: [
     {
-      id: 1,
+      id: '1',
       title: 'Hello Roman',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
@@ -40,7 +40,7 @@ const initialState = {
         'https://pbs.twimg.com/profile_images/1104491562854158336/A-NTwQhW_400x400.png',
     },
     {
-      id: 2,
+      id: '2',
       title: 'Redux guy',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
@@ -49,7 +49,7 @@ const initialState = {
         'https://pbs.twimg.com/profile_images/1336281436685541376/fRSl8uJP_400x400.jpg',
     },
     {
-      id: 3,
+      id: '3',
       title: 'React router stuff',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
@@ -58,7 +58,7 @@ const initialState = {
         'https://pbs.twimg.com/profile_images/1095819845382299649/zG-2_UHS_400x400.jpg',
     },
     {
-      id: 4,
+      id: '4',
       title: 'Super animacje!',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
@@ -69,7 +69,7 @@ const initialState = {
   ],
   articles: [
     {
-      id: 1,
+      id: '1',
       title: 'React on my mind',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
@@ -77,7 +77,7 @@ const initialState = {
       created: '1 day',
     },
     {
-      id: 2,
+      id: '2',
       title: 'Wish you React',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
@@ -85,7 +85,7 @@ const initialState = {
       created: '1 day',
     },
     {
-      id: 3,
+      id: '3',
       title: 'You gave React a bad name',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
@@ -97,6 +97,14 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'ADD_ITEM':
+      return {
+        ...state,
+        [action.payload.itemType]: [
+          ...state[action.payload.itemType],
+          action.payload.item,
+        ],
+      };
     case 'REMOVE_ITEM':
       return {
         ...state,
